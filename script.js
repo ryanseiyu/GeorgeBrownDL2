1bbdb13b5c69a964b8e68458ecf367003ba3ff35
Ryan Seiyu <ryan_seiyu@hotmail.com>
2024-07-20 13:09:42 -0400

    testing

--- script.js
+++ script.js
@@ -198,22 +198,3 @@ function changeConfidence () {
 document.getElementById("confidence").addEventListener("input", changeConfidence);
 
 webcamInference();
-
-window.addEventListener('resize', resizeCanvas, false);
-window.addEventListener('DOMContentLoaded', resizeCanvas, false);
-
-function resizeCanvas() {
-  const canvas = document.getElementById('video_canvas');
-  // Assuming there might be a total of 20px padding (10px on each side) as an example
-  const padding = 20; // Adjust this value based on actual padding/margin
-  const availableWidth = window.innerWidth - padding;
-
-  if (window.innerWidth < 768) {
-    canvas.width = availableWidth;
-    canvas.height = availableWidth * (3 / 4); // Maintain aspect ratio
-  } else {
-    // For larger screens, adjust if necessary to prevent overflow
-    canvas.width = Math.min(640, availableWidth); // Use the smaller of 640 or availableWidth
-    canvas.height = canvas.width * (3 / 4); // Adjust height based on the new width
-  }
-}
